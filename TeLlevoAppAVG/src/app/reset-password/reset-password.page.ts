601665a1bd9e14e5ba4ac21275725e610c3c25e1
MaxVRG <max.vargasp@duocuc.cl>
2024-09-10 18:52:37 -0300

    Version N°5
    Mejoras Visuales
    Funcion Programar Viaje

--- TeLlevoAppAVG/src/app/reset-password/reset-password.page.ts
+++ TeLlevoAppAVG/src/app/reset-password/reset-password.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
 
 
 @Component({
@@ -9,14 +11,17 @@ import { NavController } from '@ionic/angular';
 })
 export class ResetPasswordPage implements OnInit {
   username: string = '';
-  constructor(private navCtrl: NavController) { }
+  constructor(private navCtrl: NavController,private router: Router) { }
 
 
   resetPassword() {
     // Lógica para restablecer la contraseña
     this.navCtrl.navigateBack('/login');
   }
-  
+
+  goBack() {
+    this.router.navigate(['/login']); // Asegúrate de que '/login' sea la ruta correcta hacia la página de inicio de sesión
+  }
   ngOnInit() {
   }
 
